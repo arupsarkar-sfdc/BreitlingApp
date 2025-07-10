97cd76a0ef717ece686711197ecff42a8380bb60
Arup Sarkar <arup.sarkar@salesforce.com>
2025-07-09 22:07:51 -0400

    Initial Commit

---
+++ BreitlingApp/BreitlingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreitlingAppApp.swift
+//  BreitlingApp
+//
+//  Created by Arup Sarkar (TA) on 7/9/25.
+//
+
+import SwiftUI
+
+@main
+struct BreitlingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BreitlingAppTests/BreitlingAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  BreitlingAppTests.swift
+//  BreitlingAppTests
+//
+//  Created by Arup Sarkar (TA) on 7/9/25.
+//
+
+import Testing
+
+struct BreitlingAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
